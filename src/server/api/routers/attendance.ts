1608f17782bec21055c7ba4b3c4d75b04b90d70f
Marcelo Risoli <celorisoli@gmail.com>
2023-02-13 22:20:10 +0100

    add payment and check in routers

--- src/server/api/routers/attendance.ts
+++ src/server/api/routers/attendance.ts
@@ -2,10 +2,72 @@ import { z } from "zod";
 import { publicProcedure, createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const attendanceRouter = createTRPCRouter({
-  checkIn: publicProcedure.input({}).mutation(({ ctx, ...req }) => {
-    return {};
-  }),
-  checkOut: publicProcedure.input({}).mutation(({ ctx, ...req }) => {
-    return {};
+  getAttendancesForMatchday: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const attendances = await ctx.prisma.attendance.findMany({
+        where: {
+          matchdayId: input,
+        },
+      });
+      return attendances;
+    }),
+  checkIn: protectedProcedure
+    .input(
+      z.object({
+        matchdayId: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const attendance = await ctx.prisma.attendance.create({
+        data: {
+          matchdayId: input.matchdayId,
+          userId: input.userId,
+        },
+      });
+      return attendance;
+    }),
+  checkOut: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.attendance.delete({
+        where: {
+          id: input,
+        },
+      });
+      return {};
+    }),
+  markAsPaid: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.attendance.update({
+        where: { id: input },
+        data: {
+          paid: true,
+        },
+      });
+      return {};
+    }),
+  getDebts: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.attendance.findMany({
+        where: { userId: input, paid: false },
+      });
+    }),
+  getDebtors: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.attendance.findMany({
+      where: {
+        paid: false,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }),
 });
